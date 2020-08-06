282c3fed66495b87d669395cee416706f6be50b9
David Castro Castaneda <dcastrocast@gmail.com>
2020-08-06 02:28:15 -0700

    add view

--- server.js
+++ server.js
@@ -14,6 +14,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 mongoose.connect('mongodb://localhost/workout', {
+  useUnifiedTopology: true,
   useNewUrlParser: true,
   useFindAndModify: false,
 });
